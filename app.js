ac0b7ecec4c89623bc9804ab962e82abcc67c3da
Borys Ushakov <inlvert@gmail.com>
2024-02-16 22:59:31 +0200

    add use(router)

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const { validateUser } = require("./middlewares/validate.mw");
 const UserController = require("./controllers/user.controller");
 
 const app = express();
+const router = express.Router()
+
+app.use(router);
 
 const PORT = 5000;
 app.listen(PORT, () => {
@@ -11,7 +14,7 @@ app.listen(PORT, () => {
 
 const bodyParser = express.json();
 
-app
+router
   .route("/users")
   .get(UserController.getUsers)
   .post(bodyParser, validateUser, UserController.createUsers);
@@ -19,18 +22,16 @@ app
 // app.get("/users", UserController.getUsers);
 // app.post("/users", bodyParser, validateUser, UserController.createUsers);
 
-app.get("/user", UserController.getUserQuery);
+router.get("/user", UserController.getUserQuery);
 
-app
+router
   .route("/users/:userId")
   .delete(UserController.deleteUser)
   .get(UserController.getUser)
   .put(bodyParser, UserController.updateUser);
 
 // app.delete("/users/:userId", UserController.deleteUser);
-
 // app.get("/users/:userId", UserController.getUser);
-
 // app.put("/users/:userId", bodyParser, UserController.updateUser);
 
 app.get("/users/:userId/message/:messageId", async (req, res, next) => {
