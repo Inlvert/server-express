8e5ae1b7352fb57cd5679023f51b4ff8111a4974
Borys Ushakov <inlvert@gmail.com>
2024-02-17 01:23:12 +0200

    all routers through '/api'

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const router = require("./routers/index");
 
 const app = express();
 
-app.use(express.json());
-app.use(router);
-
+app.use(express.json()); // const bodyParser = express.json();\\
+app.use('/api', router); // all routers through '/api'
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`SERVER STARTED ON ${PORT}`);
 });
 
-// const bodyParser = express.json();
+
 
 app.get("/users/:userId/message/:messageId", async (req, res, next) => {
   res.send(req.params);
