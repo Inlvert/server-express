7ce415287dd7c3140562c9b1d3c67d89eb8f86af
Borys Ushakov <inlvert@gmail.com>
2024-09-28 21:05:05 +0300

    add

--- routers/index.js
+++ routers/index.js
@@ -2,24 +2,23 @@ const express = require("express");
 const { validateUser } = require("../middlewares/validate.mw");
 const UserController = require("../controllers/user.controller");
 const router = express.Router();
-const multer  = require('multer');
-const path = require('path');
-
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve(__dirname, '..', 'public', 'images'))
+    cb(null, path.resolve(__dirname, "..", "public", "images"));
   },
   filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`)
-  }
-})
-const upload = multer({ storage })
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+const upload = multer({ storage });
 
 router
   .route("/users")
   .get(UserController.getUsers)
-  .post(upload.single('avatar'), validateUser, UserController.createUsers);
+  .post(upload.single("avatar"), validateUser, UserController.createUsers);
 
 // app.get("/users", UserController.getUsers);
 // app.post("/users", bodyParser, validateUser, UserController.createUsers);
@@ -30,7 +29,7 @@ router
   .route("/users/:userId")
   .delete(UserController.deleteUser)
   .get(UserController.getUser)
-  .put(upload.single('avatar'), UserController.updateUser);
+  .put(upload.single("avatar"), UserController.updateUser);
 
 // app.delete("/users/:userId", UserController.deleteUser);
 // app.get("/users/:userId", UserController.getUser);
